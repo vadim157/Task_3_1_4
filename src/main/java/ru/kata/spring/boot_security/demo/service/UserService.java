f22412de3d28fd3a0173a096e78ee811435d38ee
Vadim Trofimets <java-developer@bk.ru>
2022-07-25 17:21:43 +0300

    compile

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -1,77 +1,18 @@
 package ru.kata.spring.boot_security.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.repository.RoleRepository;
-import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
-@Service
-@Transactional(readOnly = true)
-public class UserService implements UserDetailsService {
+public interface UserService extends UserDetailsService {
 
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    List<User> findAll();
+    User show(Long id);
+    void save(User user);
+    void update(Long id, User updateUser);
+    void delete(Long id);
 
-    @Autowired
-    public UserService(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        return user;
-    }
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
-
-    public User show(Long id) {
-        Optional<User> foundUser = userRepository.findById(id);
-        return foundUser.orElse(null);
-    }
-
-    @Transactional
-    public boolean save(User user) {
-//        User userFromBD = userRepository.findByUsername(user.getUsername());
-//        if (userFromBD == null) {
-//            return false;
-//        }
-        user.setRoles(Collections.singletonList(new Role(1L, "ROLE_USER")));
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return true;
-    }
-
-    @Transactional
-    public void update(Long id, User updateUser) {
-
-        updateUser.setId(id);
-        userRepository.save(updateUser);
-    }
-
-    @Transactional
-    public void delete(Long id) {
-        userRepository.deleteById(id);
-    }
 }
-
-
