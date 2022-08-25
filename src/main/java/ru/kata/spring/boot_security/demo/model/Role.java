ba2f8d44d3a002ae4051091663cdf9d246b01b0c
Vadim Trofimets <java-developer@bk.ru>
2022-08-25 11:38:15 +0300

    Commit

--- src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -40,7 +40,6 @@ public class Role implements GrantedAuthority {
         this.name = name;
     }
 
-
     @Override
     public String toString() {
         return name;
@@ -50,7 +49,6 @@ public class Role implements GrantedAuthority {
     public String getAuthority() {
         return getName();
     }
-
 
     @Override
     public boolean equals(Object o) {
