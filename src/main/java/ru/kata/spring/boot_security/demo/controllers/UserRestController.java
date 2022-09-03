83ad22cc326e89e066080249ba2bfaa5876dacdd
Vadim Trofimets <java-developer@bk.ru>
2022-09-03 11:51:57 +0300

    Commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserRestController {
 
-    UserService userService;
+   private final UserService userService;
 
     @Autowired
     public UserRestController(UserService userService) {
